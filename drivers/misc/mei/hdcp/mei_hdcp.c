93d2eef2a7bf3ff3011a634decf608c8e1076d1f
Ramalingam C <ramalingam.c@intel.com>
2017-09-09 02:17:48 +0530

    misc/mei/hdcp: Verify M_prime
    Request to ME to verify the M_Prime received from the HDCP sink.
    
    ME FW will calculate the M and compare with M_prime received
    as part of RepeaterAuth_Stream_Ready, which is HDCP2.2 protocol msg.
    
    On successful completion of this stage, downstream propagation of
    the stream management info is completed.
    
    v2:
      Rebased.
    v3:
      cldev is passed as first parameter [Tomas]
      Redundant comments and cast are removed [Tomas]
    v4:
      %zd for ssize_t [Alexander]
      %s/return -1/return -EIO [Alexander]
      endianness conversion func is moved to drm_hdcp.h [Uma]
    v5:
      Rebased.
    v6:
      No change.
    v7:
      Collected the Rb-ed by.
      Rebasing.
    
    Signed-off-by: Ramalingam C <ramalingam.c@intel.com>
    Reviewed-by: Uma Shankar <uma.shankar@intel.com>

--- drivers/misc/mei/hdcp/mei_hdcp.c
+++ drivers/misc/mei/hdcp/mei_hdcp.c
@@ -533,6 +533,74 @@ mei_repeater_check_flow_prepare_ack(struct mei_cl_device *cldev,
 	return 0;
 }
 
+/*
+ * mei_verify_mprime:
+ *	Function to verify mprime.
+ *
+ * cldev		: Pointer for mei client device
+ * data			: Intel HW specific Data
+ * stream_ready		: pointer for RepeaterAuth_Stream_Ready message.
+ *
+ * Returns 0 on Success, <0 on Failure
+ */
+static int mei_verify_mprime(struct mei_cl_device *cldev,
+			     struct mei_hdcp_data *data,
+			     struct hdcp2_rep_stream_ready *stream_ready)
+{
+	struct wired_cmd_repeater_auth_stream_req_in
+					verify_mprime_in = { { 0 } };
+	struct wired_cmd_repeater_auth_stream_req_out
+					verify_mprime_out = { { 0 } };
+	struct device *dev;
+	ssize_t byte;
+
+	if (!stream_ready || !data)
+		return -EINVAL;
+
+	dev = &cldev->dev;
+
+	verify_mprime_in.header.api_version = HDCP_API_VERSION;
+	verify_mprime_in.header.command_id = WIRED_REPEATER_AUTH_STREAM_REQ;
+	verify_mprime_in.header.status = ME_HDCP_STATUS_SUCCESS;
+	verify_mprime_in.header.buffer_len =
+			WIRED_CMD_BUF_LEN_REPEATER_AUTH_STREAM_REQ_MIN_IN;
+
+	verify_mprime_in.port.integrated_port_type = data->port_type;
+	verify_mprime_in.port.physical_port = data->port;
+
+	memcpy(verify_mprime_in.m_prime, stream_ready->m_prime,
+	       HDCP_2_2_MPRIME_LEN);
+	reverse_endianness((u8 *)&verify_mprime_in.seq_num_m,
+			   HDCP_2_2_SEQ_NUM_LEN, (u8 *)&data->seq_num_m);
+	memcpy(verify_mprime_in.streams, data->streams,
+	       (data->k * sizeof(struct hdcp2_streamid_type)));
+
+	verify_mprime_in.k = __swab16(data->k);
+
+	byte = mei_cldev_send(cldev, (u8 *)&verify_mprime_in,
+			      sizeof(verify_mprime_in));
+	if (byte < 0) {
+		dev_dbg(dev, "mei_cldev_send failed. %zd\n", byte);
+		return byte;
+	}
+
+	byte = mei_cldev_recv(cldev, (u8 *)&verify_mprime_out,
+			      sizeof(verify_mprime_out));
+	if (byte < 0) {
+		dev_dbg(dev, "mei_cldev_recv failed. %zd\n", byte);
+		return byte;
+	}
+
+	if (verify_mprime_out.header.status != ME_HDCP_STATUS_SUCCESS) {
+		dev_dbg(dev, "ME cmd 0x%08X failed. status: 0x%X\n",
+			WIRED_REPEATER_AUTH_STREAM_REQ,
+			verify_mprime_out.header.status);
+		return -EIO;
+	}
+
+	return 0;
+}
+
 static __attribute__((unused))
 struct i915_hdcp_component_ops mei_hdcp_ops = {
 	.owner					= THIS_MODULE,
@@ -546,7 +614,7 @@ struct i915_hdcp_component_ops mei_hdcp_ops = {
 	.get_session_key			= mei_get_session_key,
 	.repeater_check_flow_prepare_ack	=
 					mei_repeater_check_flow_prepare_ack,
-	.verify_mprime				= NULL,
+	.verify_mprime				= mei_verify_mprime,
 	.enable_hdcp_authentication		= NULL,
 	.close_hdcp_session			= NULL,
 };
