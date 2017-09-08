89a25b89cdc9934a9da2b2ff0c560743ca81bc53
Ramalingam C <ramalingam.c@intel.com>
2017-09-08 15:11:50 +0530

    misc/mei/hdcp: Initiate Wired HDCP2.2 Tx Session
    Request ME FW to start the HDCP2.2 session for an intel port.
    Prepares payloads for command WIRED_INITIATE_HDCP2_SESSION and sends
    to ME FW.
    
    On Success, ME FW will start a HDCP2.2 session for the port and
    provides the content for HDCP2.2 AKE_Init message.
    
    v2:
      Rebased.
    v3:
      cldev is add as a separate parameter [Tomas]
      Redundant comment and typecast are removed [Tomas]
    v4:
      %zd is used for size [Alexander]
      %s/return -1/return -EIO [Alexander]
      Spellings in commit msg is fixed [Uma]
    v5:
      Rebased.
    v6:
      No change.
    v7:
      Collected the rb-ed by.
      Realigning the patches in the series.
    
    Signed-off-by: Ramalingam C <ramalingam.c@intel.com>
    Reviewed-by: Uma Shankar <uma.shankar@intel.com>

--- drivers/misc/mei/hdcp/mei_hdcp.c
+++ drivers/misc/mei/hdcp/mei_hdcp.c
@@ -12,6 +12,91 @@
 #include <linux/slab.h>
 #include <linux/uuid.h>
 #include <linux/mei_cl_bus.h>
+#include <linux/mei_hdcp.h>
+#include <drm/drm_connector.h>
+#include <drm/i915_component.h>
+
+#include "mei_hdcp.h"
+
+/*
+ * mei_initiate_hdcp2_session:
+ *	Function to start a Wired HDCP2.2 Tx Session with ME FW
+ *
+ * cldev		: Pointer for mei client device
+ * data			: Intel HW specific Data
+ * ake_data		: ptr to store AKE_Init
+ *
+ * Returns 0 on Success, <0 on Failure.
+ */
+static int mei_initiate_hdcp2_session(struct mei_cl_device *cldev,
+				      struct mei_hdcp_data *data,
+				      struct hdcp2_ake_init *ake_data)
+{
+	struct wired_cmd_initiate_hdcp2_session_in session_init_in = { { 0 } };
+	struct wired_cmd_initiate_hdcp2_session_out
+						session_init_out = { { 0 } };
+	struct device *dev;
+	ssize_t byte;
+
+	if (!data || !ake_data)
+		return -EINVAL;
+
+	dev = &cldev->dev;
+
+	session_init_in.header.api_version = HDCP_API_VERSION;
+	session_init_in.header.command_id = WIRED_INITIATE_HDCP2_SESSION;
+	session_init_in.header.status = ME_HDCP_STATUS_SUCCESS;
+	session_init_in.header.buffer_len =
+				WIRED_CMD_BUF_LEN_INITIATE_HDCP2_SESSION_IN;
+
+	session_init_in.port.integrated_port_type = data->port_type;
+	session_init_in.port.physical_port = data->port;
+	session_init_in.protocol = (uint8_t)data->protocol;
+
+	byte = mei_cldev_send(cldev, (u8 *)&session_init_in,
+			      sizeof(session_init_in));
+	if (byte < 0) {
+		dev_dbg(dev, "mei_cldev_send failed. %zd\n", byte);
+		return byte;
+	}
+
+	byte = mei_cldev_recv(cldev, (u8 *)&session_init_out,
+			      sizeof(session_init_out));
+	if (byte < 0) {
+		dev_dbg(dev, "mei_cldev_recv failed. %zd\n", byte);
+		return byte;
+	}
+
+	if (session_init_out.header.status != ME_HDCP_STATUS_SUCCESS) {
+		dev_dbg(dev, "ME cmd 0x%08X Failed. Status: 0x%X\n",
+			WIRED_INITIATE_HDCP2_SESSION,
+			session_init_out.header.status);
+		return -EIO;
+	}
+
+	ake_data->msg_id = HDCP_2_2_AKE_INIT;
+	ake_data->tx_caps = session_init_out.tx_caps;
+	memcpy(ake_data->r_tx, session_init_out.r_tx,
+	       sizeof(session_init_out.r_tx));
+
+	return 0;
+}
+
+static __attribute__((unused))
+struct i915_hdcp_component_ops mei_hdcp_ops = {
+	.owner					= THIS_MODULE,
+	.initiate_hdcp2_session			= mei_initiate_hdcp2_session,
+	.verify_receiver_cert_prepare_km	= NULL,
+	.verify_hprime				= NULL,
+	.store_pairing_info			= NULL,
+	.initiate_locality_check		= NULL,
+	.verify_lprime				= NULL,
+	.get_session_key			= NULL,
+	.repeater_check_flow_prepare_ack	= NULL,
+	.verify_mprime				= NULL,
+	.enable_hdcp_authentication		= NULL,
+	.close_hdcp_session			= NULL,
+};
 
 static int mei_hdcp_probe(struct mei_cl_device *cldev,
 			  const struct mei_cl_device_id *id)
