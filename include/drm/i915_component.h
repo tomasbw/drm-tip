f2f970de793f96574044d416c382040ba5694c24
Ramalingam C <ramalingam.c@intel.com>
2018-07-11 18:57:39 +0530

    drm/i915: component master at i915 driver load
    A generic component master is added to hold the i915 registration
    until all required kernel modules are up and active.
    
    This is achieved through following steps:
      - moving the i915 driver registration to the component master's
        bind call
      - all required kernel modules will add one component each to
        component_match of I915 component master.
    
    If no component is added to the I915 component master, due to CONFIG
    selection or HW limitation, component master's bind call (i915
    registration) will be triggered with no wait.
    
    Similarly when a associated component is removed for some reasons,
    I915 will be unregistered through component master unbind.
    
    v2:
      i915_driver_unregister is added to the unbind of master.
    
    Signed-off-by: Ramalingam C <ramalingam.c@intel.com>
    Suggested-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- include/drm/i915_component.h
+++ include/drm/i915_component.h
@@ -46,4 +46,20 @@ struct i915_audio_component {
 	int aud_sample_rate[MAX_PORTS];
 };
 
+/**
+ * struct i915_component_master - Used for communication between i915
+ * and any other drivers for the services of different feature.
+ */
+struct i915_component_master {
+	/**
+	 * @i915_kdev: Kdev of I915. Used from the client component for
+	 * removing the reference to mei_cldev.
+	 */
+	struct device *i915_kdev;
+
+	/*
+	 * Add here the interface details between I915 and interested modules.
+	 */
+};
+
 #endif /* _I915_COMPONENT_H_ */
